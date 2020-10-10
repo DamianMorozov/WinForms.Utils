5799ee4ac281a605d7891c32fa2fac10d7935720
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-10-10 08:52:47 +0300

    Added InvokeControl

---
+++ WinForms.Utils/InvokeControl.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms.Utils
+{
+    public static class InvokeControl
+    {
+        public static void SetText(Control control, string value)
+        {
+            void Work(Control inControl, string inValue)
+            {
+                inControl.Text = inValue;
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control, value);
+                    }));
+                }
+                else
+                {
+                    Work(control, value);
+                }
+            }
+        }
+
+        public static void SetVisible(Control control, bool value)
+        {
+            void Work(Control inControl, bool inValue)
+            {
+                inControl.Visible = inValue;
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control, value);
+                    }));
+                }
+                else
+                {
+                    Work(control, value);
+                }
+            }
+        }
+
+        public static void SetEnabled(Control control, bool value)
+        {
+            void Work(Control inControl, bool inValue)
+            {
+                inControl.Enabled = inValue;
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control, value);
+                    }));
+                }
+                else
+                {
+                    Work(control, value);
+                }
+            }
+        }
+
+        public static void SetBackColor(Control control, Color value)
+        {
+            void Work(Control inControl, Color inValue)
+            {
+                inControl.BackColor = inValue;
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control, value);
+                    }));
+                }
+                else
+                {
+                    Work(control, value);
+                }
+            }
+        }
+
+        public static void SetForeColor(Control control, Color value)
+        {
+            void Work(Control inControl, Color inValue)
+            {
+                inControl.ForeColor = inValue;
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control, value);
+                    }));
+                }
+                else
+                {
+                    Work(control, value);
+                }
+            }
+        }
+
+        public static void Select(Control control)
+        {
+            void Work(Control inControl)
+            {
+                inControl.Select();
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control);
+                    }));
+                }
+                else
+                {
+                    Work(control);
+                }
+            }
+        }
+
+        public static void Focus(Control control)
+        {
+            void Work(Control inControl)
+            {
+                inControl.Focus();
+            }
+
+            if (control != null)
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Action(() =>
+                    {
+                        Work(control);
+                    }));
+                }
+                else
+                {
+                    Work(control);
+                }
+            }
+        }
+    }
+}
