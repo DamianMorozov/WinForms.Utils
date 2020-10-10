f231e4051e722a75dd6c05ac5f599911135d0b75
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-10-10 09:13:51 +0300

    Changed EnumValues

--- WinForms.Utils.Tests/EnumValues.cs
+++ WinForms.Utils.Tests/EnumValues.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Drawing;
 // ReSharper disable UnusedMember.Global
 // Last changed 2020-10-10.
 
@@ -71,15 +70,6 @@ namespace WinForms.Utils.Tests
         public static List<int> GetInt()
         {
             return new List<int>() { int.MinValue, 1, int.MaxValue / 2, int.MaxValue };
-        }
-
-        /// <summary>
-        /// List of GetColor values.
-        /// </summary>
-        /// <returns></returns>
-        public static List<Color> GetColor()
-        {
-            return new List<Color>() { Color.White, Color.Black, Color.Blue, Color.Gray, Color.Green, Color.Red };
         }
 
         /// <summary>
