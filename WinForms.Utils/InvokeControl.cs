7837d24a5487bf3844a3367ac93b52da6654884e
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-10-16 21:23:55 +0300

    Changed InvokeControl

--- WinForms.Utils/InvokeControl.cs
+++ WinForms.Utils/InvokeControl.cs
@@ -8,185 +8,170 @@ namespace WinForms.Utils
     {
         public static void SetText(Control control, string value)
         {
+            if (control is null)
+                return;
             void Work(Control inControl, string inValue)
             {
                 inControl.Text = inValue;
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control, value);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control, value);
-                }
+                }));
+            }
+            else
+            {
+                Work(control, value);
             }
         }
 
         public static void AddText(Control control, string value)
         {
+            if (control is null)
+                return;
             void Work(Control inControl, string inValue)
             {
                 inControl.Text += inValue;
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control, value);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control, value);
-                }
+                }));
+            }
+            else
+            {
+                Work(control, value);
             }
         }
 
         public static void SetVisible(Control control, bool value)
         {
+            if (control is null)
+                return;
             void Work(Control inControl, bool inValue)
             {
                 inControl.Visible = inValue;
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control, value);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control, value);
-                }
+                }));
+            }
+            else
+            {
+                Work(control, value);
             }
         }
 
         public static void SetEnabled(Control control, bool value)
         {
+            if (control is null)
+                return;
             void Work(Control inControl, bool inValue)
             {
                 inControl.Enabled = inValue;
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control, value);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control, value);
-                }
+                }));
+            }
+            else
+            {
+                Work(control, value);
             }
         }
 
         public static void SetBackColor(Control control, Color value)
         {
+            if (control is null)
+                return;
             void Work(Control inControl, Color inValue)
             {
                 inControl.BackColor = inValue;
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control, value);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control, value);
-                }
+                }));
+            }
+            else
+            {
+                Work(control, value);
             }
         }
 
         public static void SetForeColor(Control control, Color value)
         {
+            if (control is null)
+                return;
             void Work(Control inControl, Color inValue)
             {
                 inControl.ForeColor = inValue;
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control, value);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control, value);
-                }
+                }));
+            }
+            else
+            {
+                Work(control, value);
             }
         }
 
         public static void Select(Control control)
         {
+            if (control is null)
+                return;
             void Work(Control inControl)
             {
                 inControl.Select();
             }
-
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control);
-                }
+                }));
+            }
+            else
+            {
+                Work(control);
             }
         }
 
         public static void Focus(Control control)
         {
+            if (control is null)
+                return;
             void Work(Control inControl)
             {
                 inControl.Focus();
             }
 
-            if (control != null)
+            if (control.InvokeRequired)
             {
-                if (control.InvokeRequired)
-                {
-                    control.Invoke(new Action(() =>
-                    {
-                        Work(control);
-                    }));
-                }
-                else
+                control.Invoke(new Action(() =>
                 {
                     Work(control);
-                }
+                }));
+            }
+            else
+            {
+                Work(control);
             }
         }
     }
